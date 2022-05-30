19e6bac52d8be0f6dd2d5e21e418363df57cc74b
Tomáš Vodička <vodictomas@gmail.com>
2022-05-30 12:02:57 +0200

    tests

--- src/control/SelectBox.php
+++ src/control/SelectBox.php
@@ -46,9 +46,9 @@ class SelectBox extends \Nette\Forms\Controls\SelectBox implements Renderable
 			->class('form-group row')
 			->addHtml($labelDiv . $inputDiv);
 
-		if($input->getOption('id'))
+		if($this->getOption('id'))
 		{
-			$outerDiv->id($input->getOption('id'));
+			$outerDiv->id($this->getOption('id'));
 		}
 
 		return $outerDiv;
