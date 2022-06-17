2e70de3bf9f382d546b64ae4ed5b6af742174e77
Tomáš Vodička <vodictomas@gmail.com>
2022-06-17 13:51:06 +0200

    icon macro

--- src/helper/CoreList.php
+++ src/helper/CoreList.php
@@ -40,7 +40,7 @@ trait CoreList
 				$tooltip = Html::el('span')
 					->title($this->tooltips[$key])
 					->addAttributes(['data-placement' => 'top', 'data-toggle' => 'tooltip'])
-					->addHtml(\Kravcik\LatteFontAwesomeIcon\Extension::render('question-circle', ['color' => 'blue']));
+					->addHtml(\Kravcik\LatteFontAwesomeIcon\Extension::render('question-circle', color: 'blue'));
 			}
 
 			$class = 'form-check-inline mr-0 col-' . 12 / $this->itemsPerRow;
