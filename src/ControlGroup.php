562882b643d8db6173996107680ff5f4f58be771
Tomáš Vodička <vodictomas@gmail.com>
2022-06-30 13:48:59 +0200

    #5 remove box

---
+++ src/ControlGroup.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ModulIS\Form;
+
+class ControlGroup extends \Nette\Forms\ControlGroup
+{
+	public function getInputArray(): array
+	{
+		$controlArray = [];
+
+		/**
+		 * Skip submitters
+		 */
+		foreach($this->getControls() as $control)
+		{
+			if($control instanceof Control\Button || $control instanceof Control\SubmitButton || $control instanceof Control\Link)
+			{
+				continue;
+			}
+
+			$controlArray[] = $control;
+		}
+
+		return $controlArray;
+	}
+
+
+	public function getSubmitterArray(): array
+	{
+		$controlArray = [];
+
+		/**
+		 * Only submitters
+		 */
+		foreach($this->getControls() as $control)
+		{
+			if($control instanceof Control\Button || $control instanceof Control\SubmitButton || $control instanceof Control\Link)
+			{
+				$controlArray[] = $control;
+			}
+		}
+
+		return $controlArray;
+	}
+	
+	
+	public function setColor(string $color): self
+	{
+		return $this->setOption('color', $color);
+	}
+}
