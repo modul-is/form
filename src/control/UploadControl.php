0a4d902ec767b5c57831304af905e5e7d326e76f
Tomáš Vodička <vodictomas@gmail.com>
2022-07-11 12:35:31 +0200

    tests

--- src/control/UploadControl.php
+++ src/control/UploadControl.php
@@ -32,8 +32,20 @@ class UploadControl extends \Nette\Forms\Controls\UploadControl implements Rende
 		$currentClass = $input->getAttribute('class') ? ' ' . $input->getAttribute('class') : '';
 
 		$input->addAttributes(['class' => 'form-control' . $currentClass . $validationClass]);
+		
+		$prepend = $this->getPrepend();
+		$append = $this->getAppend();
+		
+		if($prepend || $append)
+		{
+			$inputGroup =  Html::el('div')
+				->class('input-group')
+				->addHtml($prepend . $input . $append);
+			
+			return $inputGroup . $validationFeedBack;
+		}
 
-		return $this->getPrepend() . $input . $this->getAppend() . $validationFeedBack;
+		return  $input . $validationFeedBack;
 	}
 
 
