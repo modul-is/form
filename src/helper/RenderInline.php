f7de76083ce19ad5ebfdc8829576a607ffb1bcb1
Tomáš Vodička <vodictomas@gmail.com>
2026-07-09 19:58:38 +0200

    return

--- src/helper/RenderInline.php
+++ src/helper/RenderInline.php
@@ -19,7 +19,7 @@ trait RenderInline
 	protected ?Html $wrapControl = null;
 
 
-	public function setLabelWrapClass(string $class): self
+	public function setLabelWrapClass(string $class): static
 	{
 		$this->labelClass = $class;
 
@@ -33,7 +33,7 @@ trait RenderInline
 	}
 
 
-	public function setInputWrapClass(string $class): self
+	public function setInputWrapClass(string $class): static
 	{
 		$this->inputClass = $class;
 
@@ -47,7 +47,7 @@ trait RenderInline
 	}
 
 
-	public function setRowClass(string $class): self
+	public function setRowClass(string $class): static
 	{
 		$this->rowClass = $class;
 
@@ -55,7 +55,7 @@ trait RenderInline
 	}
 
 
-	public function setWrapClass(string $class): self
+	public function setWrapClass(string $class): static
 	{
 		$this->getWrapControl()
 			->setAttribute('class', $class);
@@ -64,7 +64,7 @@ trait RenderInline
 	}
 
 
-	public function setWrapId(string $id): self
+	public function setWrapId(string $id): static
 	{
 		$this->getWrapControl()
 			->setAttribute('id', $id);
