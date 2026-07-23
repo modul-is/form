7b36d119681f69c416eb3833c7965c56d815b7af
krumld <david.kruml@modulservis.cz>
2026-07-23 14:41:05 +0200

    whisperer on paste input

--- src/js/whisperer.js
+++ src/js/whisperer.js
@@ -61,108 +61,117 @@
 					$('#' + chosenId).find('li.no-results').html('<span class="color-black"><i class="fal fa-spinner fa-spin"></i>&nbsp;&nbsp;Načítají se položky</span>');
 				});
 
-				$(document).on('keydown', '#' + chosenId + ' input.chosen-search-input', function(event)
+				var runWhisper = function(searchInput)
 				{
-					var searchInput = $(this);
-					var code = (event.keyCode || event.which);
+					clearTimeout(typingTimer);
 
-					if(jQuery.inArray(code, disallowedKeyArray) === -1)
+					typingTimer = setTimeout(function()
 					{
-						clearTimeout(typingTimer);
-
-						typingTimer = setTimeout(function()
+						var param = searchInput.val();
+						if(param.length < 1)
 						{
-							var param = searchInput.val();
-							if(param.length < 1)
+							return;
+						}
+
+						if(currentAbortController)
+						{
+							currentAbortController.abort();
+						}
+						currentAbortController = new AbortController();
+
+						var parents = element.data('dependentselectbox-parents');
+						var parentArray = {};
+
+						$.each(parents || {}, function(name, id)
+						{
+							var parentElement = $('#' + id);
+							if(parentElement.length > 0)
+							{
+								var val;
+								if(parentElement.prop('type') === 'checkbox')
+								{
+									val = parentElement.prop('checked') ? 1 : 0;
+								}
+								else
+								{
+									val = parentElement.val();
+								}
+								parentArray[name] = val;
+							}
+							else if($("[id^='" + id + "']").length > 0)
+							{
+								parentArray[name] = $("[id^='" + id + "']:checked").val();
+							}
+						});
+
+						naja.makeRequest('POST', varUrlOnChange,
+							{param: param, parent: parentArray},
+							{
+								dataType: "json",
+								signal: currentAbortController.signal
+							}
+						).then(function(response)
+						{
+							if(searchInput.val() !== param)
 							{
 								return;
 							}
 
-							if(currentAbortController)
+							var empty = true;
+							element.empty();
+
+							$.each(response.suggestions || [], function(index, el)
 							{
-								currentAbortController.abort();
+								if(el.data !== "")
+								{
+									empty = false;
+								}
+								element.append($('<option>', {value: el.data, text: el.value}));
+							});
+
+							element.trigger("chosen:updated");
+
+							var refreshedContainer = $('#' + chosenId);
+							refreshedContainer.addClass('chosen-with-drop chosen-container-active');
+
+							var refreshedInput = refreshedContainer.find('input.chosen-search-input');
+							refreshedInput.val(param);
+							refreshedInput.focus();
+
+							if(empty === true)
+							{
+								var editedId = chosenId.replaceAll('_', '-');
+								var lastDashIndex = editedId.lastIndexOf('-');
+								var resultId = editedId.substring(0, lastDashIndex);
+
+								var message = $('#' + resultId).attr('no-result-message')
+									?? 'Nebyla nalezena žádná položka - ' + param;
+
+								refreshedContainer.find('ul.chosen-results').append($('<li class="no-results">').text(message));
 							}
-							currentAbortController = new AbortController();
-
-							var parents = element.data('dependentselectbox-parents');
-							var parentArray = {};
-
-							$.each(parents || {}, function(name, id)
+						}).catch(function(err)
+						{
+							if(err.name !== 'AbortError')
 							{
-								var parentElement = $('#' + id);
-								if(parentElement.length > 0)
-								{
-									var val;
-									if(parentElement.prop('type') === 'checkbox')
-									{
-										val = parentElement.prop('checked') ? 1 : 0;
-									}
-									else
-									{
-										val = parentElement.val();
-									}
-									parentArray[name] = val;
-								}
-								else if($("[id^='" + id + "']").length > 0)
-								{
-									parentArray[name] = $("[id^='" + id + "']:checked").val();
-								}
-							});
+								console.error(err);
+							}
+						});
+					}, delay);
+				};
 
-							naja.makeRequest('POST', varUrlOnChange,
-								{param: param, parent: parentArray},
-								{
-									dataType: "json",
-									signal: currentAbortController.signal
-								}
-							).then(function(response)
-							{
-								if(searchInput.val() !== param)
-								{
-									return;
-								}
+				$(document).on('keydown', '#' + chosenId + ' input.chosen-search-input', function(event)
+				{
+					var code = (event.keyCode || event.which);
 
-								var empty = true;
-								element.empty();
-
-								$.each(response.suggestions || [], function(index, el)
-								{
-									if(el.data !== "")
-									{
-										empty = false;
-									}
-									element.append($('<option>', {value: el.data, text: el.value}));
-								});
-
-								element.trigger("chosen:updated");
-
-								var refreshedContainer = $('#' + chosenId);
-								refreshedContainer.addClass('chosen-with-drop chosen-container-active');
-
-								var refreshedInput = refreshedContainer.find('input.chosen-search-input');
-								refreshedInput.val(param);
-								refreshedInput.focus();
-
-								if(empty === true)
-								{
-									var editedId = chosenId.replaceAll('_', '-');
-									var lastDashIndex = editedId.lastIndexOf('-');
-									var resultId = editedId.substring(0, lastDashIndex);
-
-									var message = $('#' + resultId).attr('no-result-message')
-										?? 'Nebyla nalezena žádná položka - ' + param;
-
-									refreshedContainer.find('ul.chosen-results').append($('<li class="no-results">').text(message));
-								}
-							}).catch(function(err)
-							{
-								if(err.name !== 'AbortError')
-								{
-									console.error(err);
-								}
-							});
-						}, delay);
+					if(jQuery.inArray(code, disallowedKeyArray) === -1)
+					{
+						runWhisper($(this));
 					}
+				});
+
+				$(document).on('paste input', '#' + chosenId + ' input.chosen-search-input', function()
+				{
+					runWhisper($(this));
 				});
 			}
 		});
