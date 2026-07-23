73be4221f750bef1d0863e6cf7449fb4e508a218
krumld <david.kruml@modulservis.cz>
2026-07-23 12:48:19 +0200

    rm inline-script

--- src/control/SelectBox.php
+++ src/control/SelectBox.php
@@ -61,7 +61,7 @@ class SelectBox extends \Nette\Forms\Controls\SelectBox implements Renderable, H
 
 			$imageDiv = Html::el('div')
 				->id($this->getHtmlId() . '-select2')
-				->style('display:none;');
+				->class('d-none');
 
 			foreach($this->getItems() as $key => $value)
 			{
