7132b195ed7b7cf19808ca6971c6d53e064c2d29
Tomáš Vodička <vodictomas@gmail.com>
2026-08-06 09:49:04 +0200

    phpstan (#133)

--- src/DuplicatorContainer.php
+++ src/DuplicatorContainer.php
@@ -8,6 +8,11 @@ use Stringable;
 
 class DuplicatorContainer extends Container
 {
+	/**
+	 * Callback dostane duplikátor a kontejner mazané položky - viz DuplicatorRemoveSubmit::addRemoveOnClick().
+	 * Bez vlastní anotace se zdědí nesouhlasící @param z Nette\Forms\Container::addSubmit().
+	 * @param ?\Closure(Control\Duplicator, DuplicatorContainer): void $callable
+	 */
 	public function addSubmit(string $name, Stringable|string|null $caption = null, $callable = null): Control\DuplicatorRemoveSubmit
 	{
 		$control = new Control\DuplicatorRemoveSubmit($caption);
